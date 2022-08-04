43bb5d092d497e5dc97f49cba29af0590a797b9e
yeremiadio <raikkonendio@gmail.com>
2022-08-04 10:06:12 +0700

    added search params

--- src/pages/Page0.tsx
+++ src/pages/Page0.tsx
@@ -1,30 +1,45 @@
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { ChangeEvent, useEffect } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 
 type Props = {};
 
 const Page0 = (props: Props) => {
   const navigate = useNavigate();
-
+  const [searchParams, setSearchParams] = useSearchParams();
+  const linkInputId = searchParams.get("linkInputId");
   const handleClickPuntenBro = () => {
     navigate("/blog");
     localStorage.setItem("namaku", "firda");
   };
 
+  const handleSelectLink = (event: ChangeEvent<HTMLSelectElement>) => {
+    searchParams.set("linkInputId", event.target.value);
+    setSearchParams(searchParams);
+  };
+
   return (
     <MainLayout>
-      <Link to={"/blog"}>
-        <button style={{ background: "blue", border: "none", color: "white" }}>
-          Punten bro
+      <select onChange={handleSelectLink}>
+        <option value={"link blog"}>link blog</option>
+        <option value={"punten bro tanpa link"}>punten bro tanpa link</option>
+      </select>
+      {linkInputId !== "" && linkInputId === "link blog" ? (
+        <Link to={"/blog"}>
+          <button
+            style={{ background: "blue", border: "none", color: "white" }}
+          >
+            Punten bro
+          </button>
+        </Link>
+      ) : (
+        <button
+          style={{ background: "red", border: "none", color: "white" }}
+          onClick={handleClickPuntenBro}
+        >
+          Punten bro tanpa link
         </button>
-      </Link>
-      <button
-        style={{ background: "red", border: "none", color: "white" }}
-        onClick={handleClickPuntenBro}
-      >
-        Punten bro tanpa link
-      </button>
+      )}
     </MainLayout>
   );
 };
